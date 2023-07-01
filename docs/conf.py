355a2c5aaa23e7851c6a8107f0d816a53b93e735
marcusm117 <45340140+marcusm117@users.noreply.github.com>
2023-06-30 22:43:29 -0400

    MCTK-14: bump version to 0.1.1 (#28)
    * update .bumpversion.cfg
    
    * Bump version: 0.1.0 → 0.1.1
    
    * update build.yml

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2023, marcusm117'
 author = 'marcusm117'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
